520c97d8ba9d33220635bcd792f779a809f390a3
Lolikana <thomas.guiart@gmail.com>
2023-06-02 14:26:59 +0900

    thrown error and error msg

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -40,7 +40,7 @@ it('should throw an error if no value is passed into the function', () => {
   const resultFn = () => {
     add();
   };
-  expect(resultFn).toThrow();
+  expect(resultFn).toThrow(/is not iterable/);
 });
 
 it('should throw an error if provided with multiple arguments instead of an array', () => {
@@ -49,5 +49,5 @@ it('should throw an error if provided with multiple arguments instead of an arra
   const resultFn = () => {
     add(num1, num2);
   };
-  expect(resultFn).toThrow();
+  expect(resultFn).toThrow(/is not iterable/); // contain these words in error message
 });
