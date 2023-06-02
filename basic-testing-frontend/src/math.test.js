393fb3da8d461dda649e3d1519df9d3815174a96
Lolikana <thomas.guiart@gmail.com>
2023-06-02 13:29:27 +0900

    defining behavior

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -10,3 +10,22 @@ it('should summarize all number values in an array', () => {
   const expectedResult = numbers.reduce((preValue, curValue) => preValue + curValue, 0);
   expect(result).toBe(expectedResult);
 });
+
+it('should yield NaN if at least one invalid number is provided', () => {
+  // Arrange
+  const inputs = [1, 2, 'test'];
+  //Arc
+  const result = add(inputs);
+  // Assert
+  expect(result).toBeNaN();
+});
+
+it('should yield a correct sum if an array of numeric string values is provided', () => {
+  // Arrange
+  const numbers = ['1', '2', '3'];
+  //Arc
+  const result = add(numbers);
+  // Assert
+  const expectedResult = numbers.reduce((preValue, curValue) => +preValue + +curValue, 0);
+  expect(result).toBe(expectedResult);
+});
