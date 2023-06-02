4fd169253517b696663332fafc1c4f742dfc1c4a
Lolikana <thomas.guiart@gmail.com>
2023-06-02 15:01:40 +0900

    multiple assertions/expectations

--- basic-testing-frontend/src/util/numbers.test.js
+++ basic-testing-frontend/src/util/numbers.test.js
@@ -6,10 +6,25 @@ it('should result a number', () => {
   expect(result1).toBeTypeOf('number');
 });
 
-it('should throw an error if not numeric or numeric string value are passed', () => {
-  const value = [1, 2];
-  const resultFn1 = () => {
-    transformToNumber(value);
-  };
-  expect(resultFn1).toThrow();
+it('should result a number', () => {
+  const input = '1';
+  const result1 = transformToNumber(input);
+  expect(result1).toBe(+input);
+});
+
+// it('should throw an error if not numeric or numeric string value are passed', () => {
+//   const value = [1, 2];
+//   const resultFn1 = () => {
+//     transformToNumber(value);
+//   };
+//   expect(resultFn1).toThrow();
+// });
+
+it('should yield NaN for non-transformable values', () => {
+  const input = 'invalid';
+  const input2 = {};
+  const result = transformToNumber(input);
+  const result2 = transformToNumber(input2);
+  expect(result).toBeNaN();
+  expect(result2).toBeNaN();
 });
