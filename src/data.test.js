5c4cd334362704a51a587daf2c71e75f21ed2f5e
Lolikana <thomas.guiart@gmail.com>
2023-06-05 16:11:00 +0900

    spies

---
+++ src/data.test.js
@@ -0,0 +1,11 @@
+import { vi, it, expect, describe } from 'vitest';
+import { generateReportData } from './data';
+
+describe('generateReportData()', () => {
+  it('should execute logFn if provided', () => {
+    const logger = vi.fn();
+    generateReportData(logger);
+
+    expect(logger).toBeCalled();
+  });
+});
