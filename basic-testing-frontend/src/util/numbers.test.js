acbccb4d3e5cf4ae74c2a09aae78ac98ebc99697
Lolikana <thomas.guiart@gmail.com>
2023-06-05 10:34:53 +0900

    writing integration test

--- basic-testing-frontend/src/util/numbers.test.js
+++ basic-testing-frontend/src/util/numbers.test.js
@@ -1,30 +1,43 @@
-import { expect, it } from 'vitest';
-import { transformToNumber } from '../../../basic-testing-frontend/src/util/numbers';
+import { describe, expect, it } from 'vitest';
+import {
+  cleanNumbers,
+  transformToNumber,
+} from '../../../basic-testing-frontend/src/util/numbers';
 
-it('should result a number', () => {
-  const result1 = transformToNumber('1');
-  expect(result1).toBeTypeOf('number');
+describe('transformToNumber', () => {
+  it('should result a number', () => {
+    const result1 = transformToNumber('1');
+    expect(result1).toBeTypeOf('number');
+  });
+
+  it('should result a number', () => {
+    const input = '1';
+    const result1 = transformToNumber(input);
+    expect(result1).toBe(+input);
+  });
+
+  // it('should throw an error if not numeric or numeric string value are passed', () => {
+  //   const value = [1, 2];
+  //   const resultFn1 = () => {
+  //     transformToNumber(value);
+  //   };
+  //   expect(resultFn1).toThrow();
+  // });
+
+  it('should yield NaN for non-transformable values', () => {
+    const input = 'invalid';
+    const input2 = {};
+    const result = transformToNumber(input);
+    const result2 = transformToNumber(input2);
+    expect(result).toBeNaN();
+    expect(result2).toBeNaN();
+  });
 });
 
-it('should result a number', () => {
-  const input = '1';
-  const result1 = transformToNumber(input);
-  expect(result1).toBe(+input);
-});
-
-// it('should throw an error if not numeric or numeric string value are passed', () => {
-//   const value = [1, 2];
-//   const resultFn1 = () => {
-//     transformToNumber(value);
-//   };
-//   expect(resultFn1).toThrow();
-// });
-
-it('should yield NaN for non-transformable values', () => {
-  const input = 'invalid';
-  const input2 = {};
-  const result = transformToNumber(input);
-  const result2 = transformToNumber(input2);
-  expect(result).toBeNaN();
-  expect(result2).toBeNaN();
+describe('cleanNumbers()', () => {
+  it('should return an array of number values if an array of string number is provided', () => {
+    const numberValues = ['1', '2'];
+    const cleanedNumbers = cleanNumbers(numberValues);
+    expect(cleanedNumbers[0]).toBeTypeOf('number');
+  });
 });
