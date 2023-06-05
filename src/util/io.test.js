de67a11a8f54d87f6a016c7ebce242344cd345dc
Lolikana <thomas.guiart@gmail.com>
2023-06-05 16:21:39 +0900

    mocks

--- src/util/io.test.js
+++ src/util/io.test.js
@@ -1,8 +1,14 @@
-import { it, expect } from 'vitest';
+import { it, expect, vi } from 'vitest';
 import writeData from './io';
+import { promises as fs } from 'fs';
+
+vi.mock('fs');
 
 it('should execute the writeFile method', () => {
   const testData = 'test';
   const fileName = 'text.txt';
-  return expect(writeData(testData, fileName)).resolves.toBeUndefined();
+
+  writeData(testData, fileName);
+  // return expect(writeData(testData, fileName)).resolves.toBeUndefined();
+  expect(fs.writeFile).toBeCalled();
 });
