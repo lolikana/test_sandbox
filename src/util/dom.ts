d8be5f889d2c6839d607ba57b343294c70bc1810
Lolikana <thomas.guiart@gmail.com>
2023-06-06 16:44:51 +0900

    react-testing-TOP1 setup

--- src/util/dom.ts
+++
@@ -1,7 +0,0 @@
-export function showError(message: string) {
-  const errorContainerElement = document.getElementById('errors') as HTMLDivElement;
-  const errorMessageElement = document.createElement('p') as HTMLParagraphElement;
-  errorMessageElement.textContent = message;
-  errorContainerElement.innerHTML = '';
-  errorContainerElement.append(errorMessageElement);
-}
--- src/util/validation.ts
+++
@@ -1,7 +0,0 @@
-import { ValidationError } from './errors.js';
-
-export function validateNotEmpty(text: string | null, errorMessage: string) {
-  if (!text || text.trim().length === 0) {
-    throw new ValidationError(errorMessage);
-  }
-}
