7f68cd37a5fb90c8e73695d755501e79a333df4f
Lolikana <thomas.guiart@gmail.com>
2023-06-06 16:53:08 +0900

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist',
+    sourcemap: 'inline',
+    minify: 'esbuild',
+    emptyOutDir: true,
+    copyPublicDir: true
+  },
   plugins: [react()],
+  server: {
+    port: 3000
+  }
 });
