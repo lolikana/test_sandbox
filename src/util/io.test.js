9ae2aeb6b4e361674c2489df0de504fc315fcd96
Lolikana <thomas.guiart@gmail.com>
2023-06-06 09:02:06 +0900

    custom mocking logic

--- src/util/io.test.js
+++ src/util/io.test.js
@@ -1,14 +1,25 @@
 import { it, expect, vi } from 'vitest';
 import writeData from './io';
 import { promises as fs } from 'fs';
+import path from 'path';
 
 vi.mock('fs');
+vi.mock('path', () => {
+  return {
+    default: {
+      join: (...args) => {
+        return args[args.length - 1];
+      },
+    },
+  };
+});
 
 it('should execute the writeFile method', () => {
   const testData = 'test';
   const fileName = 'text.txt';
 
   writeData(testData, fileName);
-  // return expect(writeData(testData, fileName)).resolves.toBeUndefined();
-  expect(fs.writeFile).toBeCalled();
+  // return expect(writeData(testData, testFilename)).resolves.toBeUndefined() ;
+  // expect(fs.writeFile).toBeCalled();
+  expect(fs.writeFile).toBeCalledWith(fileName, testData);
 });
