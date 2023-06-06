01ccece7e6b26de4f2df33e6197b6eb83be09f41
Lolikana <thomas.guiart@gmail.com>
2023-06-06 14:02:32 +0900

    specific moking logic

--- src/util/http.test.ts
+++ src/util/http.test.ts
@@ -6,6 +6,9 @@ const testResponseData = { testKey: 'testData' };
 
 const testFetch = vi.fn((url, options) => {
   return new Promise((resolve, reject) => {
+    if (typeof options.body !== 'string') {
+      return reject('Not a string!');
+    }
     const testResponse = {
       ok: true,
       json() {
@@ -24,4 +27,18 @@ it('should return any available response data', () => {
   const testData = { key: 'test' };
 
   return expect(sendDataRequest(testData)).resolves.toEqual(testResponseData);
+});
+
+it('should convert the provided date to JSOn before sending the request', async () => {
+  const testData = { key: 'test' };
+  let errorMessage;
+
+	// return expect(sendDataRequest(testData)).not.rejects.toBe('Not a string.'); // Won't work 
+	
+  try {
+    await sendDataRequest(testData);
+  } catch (err) {
+    errorMessage = err;
+  }
+  expect(errorMessage).not.toBe('Not a string!');
 });
