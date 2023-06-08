1e53338957f4534575b0f237ce82e1d81f528b27
Lolikana <thomas.guiart@gmail.com>
2023-06-08 10:51:19 +0900

    test and cleanup work

--- util/dom.test.js
+++ util/dom.test.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 
-import { describe, expect, it, vi } from 'vitest';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
 import { showError } from './dom';
 import { Window } from 'happy-dom';
 
@@ -10,15 +10,28 @@ const htmlDocContent = fs.readFileSync(htmlDocPath).toString();
 
 const window = new Window();
 const document = window.document;
-document.write(htmlDocContent);
+// document.write(htmlDocContent);
 vi.stubGlobal('document', document);
+
+beforeEach(() => {
+  document.body.innerHTML = '';
+  document.write(htmlDocContent);
+});
 
 describe('dom', () => {
   it("should add an error paragraph to the id='errors' element", () => {
     showError('test');
     const errorsEl = document.getElementById('errors');
     const errorPara = errorsEl.firstElementChild;
+
     expect(errorPara).not.toBeNull();
     expect(errorPara.textContent).toMatch('test');
+  });
+
+  it('should not container an error paragraph initially', () => {
+    const errorsEl = document.getElementById('errors');
+    const errorPara = errorsEl.firstElementChild;
+
+    expect(errorPara).toBeNull();
   });
 });
